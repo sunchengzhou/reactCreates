47648366970ae0dd6090c17248c1043e01ca0554
sunchengzhou <20160220@163.com>
2019-05-27 02:44:49 +0800

    version

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 2.调用次序决定变量
 
 3.初始化可以为值或者函数
+
+## Effect
+1.代替原先的生命周期
+> after mount:     componentDidMount     dom产生
+> after update:    componentDidUpdate    dom更新
+> after Unmount:   componentWillUnmount  dom移除
--- src/App2.jsx
+++ src/App2.jsx
@@ -1,17 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-let flag = 0;
-function App() {
-    const [count, setCount] = useState(() => {
-      console.log('init count')
-      return 5;
+function Example() {
+    const [count, setCount] = useState(0);
+    const [width, setWidth] = useState(() => {
+        return document.documentElement.clientWidth
     })
+
+    const resizeWindow = () => {
+        setWidth(document.documentElement.clientWidth)
+    }
+  
+    // Similar to componentDidMount and componentDidUpdate:
+    useEffect(() => {
+      // Update the document title using the browser API
+      document.title = `${count} times`;
+    },[count]);
+
+    // Similar to componentDidMount and componentDidMount:
+    useEffect(() => {
+       console.log("i am  active here!")
+      },[]);
+
+      useEffect(() => {
+        console.log("width print here!")
+        window.addEventListener('resize', resizeWindow, 'false')
+        return function remove() {
+            console.log('remove event occured')
+            window.removeEventListener('resize', resizeWindow, 'false')
+        }
+       },[width]);
+    const Clicks = () => {
+        console.log('this is test click')
+    }
+    useEffect(() => {
+        console.log('bind click')
+        document.getElementById('test').addEventListener('click', Clicks, 'false')
+        return () => {
+            console.log('remove click')
+            document.getElementById('test').removeEventListener('click', Clicks, 'false')
+        }
+       },[]);
+
     return (
       <div>
-        <button onClick={() => setCount(count + 1)}>点击</button>
-        count:({count})
+        <p>You clicked {count} times</p>
+        <div>clientWidth:{width}</div>
+        <button onClick={() => setCount(count + 1)}>
+          Click me
+        </button>
+        {
+            count%2 === 0 ? <span id="test">test1</span> : <p id="test">test2</p>
+        }
       </div>
     );
-}
+  }
 
-export default App;
+  export default Example;
